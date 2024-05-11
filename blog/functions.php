6f88838fd71228807c504d6a29162425dc7885f9
omubose <163929537+omubose@users.noreply.github.com>
2024-05-11 11:06:18 +0900

    Add files via upload

---
+++ blog/functions.php
@@ -0,0 +1,66 @@
+<?php
+
+function my_enqueue_styles() {
+  wp_enqueue_style('ress', '//unpkg.com/ress/dist/ress.min.css', array(), false, 'all');
+  wp_enqueue_style('style', get_stylesheet_uri(), array('ress'), false, 'all');
+}
+add_action('wp_enqueue_scripts', 'my_enqueue_styles');
+
+
+function st_enqueue_scripts() {
+  wp_deregister_script('jquery');
+  wp_enqueue_script('jquery', '//ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js', array(), '3.5.1', false);
+  wp_enqueue_script('main', get_theme_file_uri('js/main.js'), array('jquery'), false, true);
+}
+add_action('wp_enqueue_scripts', 'st_enqueue_scripts');
+
+add_theme_support('post-thumbnails');
+
+
+// ページネーション
+function pagination($pages = '', $range = 2) {
+  $showitems = ($range * 2) + 1;
+
+  // 現在のページ数
+  global $paged;
+  if(empty($paged)) {
+    $paged = 1;
+  }
+
+  // 全ページ数
+  if($pages == '') {
+    global $wp_query;
+    $pages = $wp_query->max_num_pages;
+    if(!$pages) {
+      $pages = 1;
+    }
+  }
+
+  // ページ数が2ぺージ以上の場合のみ、ページネーションを表示
+  if(1 != $pages) {
+    echo '<div class="pagination">';
+    echo '<ul>';
+    // 1ページ目でなければ、「前のページ」リンクを表示
+    if($paged > 1) {
+      echo '<li class="prev"><a href="' . esc_url(get_pagenum_link($paged - 1)) . '">前のページ</a></li>';
+    }
+
+    // ページ番号を表示（現在のページはリンクにしない）
+    for ($i=1; $i <= $pages; $i++) {
+      if (1 != $pages &&(!($i >= $paged+$range+1 || $i <= $paged-$range-1) || $pages <= $showitems )) {
+        if ($paged == $i) {
+          echo '<li class="active">' .$i. '</li>';
+        } else {
+          echo '<li><a href="' . esc_url(get_pagenum_link($i)) . '">' .$i. '</a></li>';
+        }
+      }
+    }
+
+    // 最終ページでなければ、「次のページ」リンクを表示
+    if ($paged < $pages) {
+      echo '<li class="next"><a href="' . esc_url(get_pagenum_link($paged + 1)) . '">次のページ</a></li>';
+    }
+    echo '</ul>';
+    echo '</div>';
+  }
+}
---
+++ blog/header.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+    <title><?php echo bloginfo('name'); ?></title>
+    <meta name="description" content="テキストテキストテキストテキストテキストテキストテキスト">
+    <meta name="viewport" content="<?php bloginfo('description'); ?>">
+    <link rel="icon" href="<?php echo esc_url(get_theme_file_uri('/img/favicon.ico')); ?>">   
+
+    <?php wp_head(); ?>
+  </head>
+
+  <body>
+  <header id="header" class="wrapper">
+    <h1 class="site-title">
+      <a href="<?php echo esc_url(home_url('/')); ?>">
+        <img src="<?php echo esc_url(get_theme_file_uri('/img/logo.svg')); ?>" alt="Furniture Design">
+      </a>
+    </h1>
+
+    <nav id="navi">
+      <ul class="nav-menu">
+        <li><a href="<?php echo esc_url(home_url('/category/products')); ?>">PRODUCTS</a></li>
+        <li><a href="<?php echo esc_url(home_url('/about')); ?>">ABOUT</a></li>
+        <li><a href="<?php echo esc_url(home_url('/company')); ?>">COMPANY</a></li>
+        <li><a href="mailto:xxxxx@xxx.xxx.com?subject=お問い合わせ">CONTACT</a></li>
+      </ul>
+    </nav>
+
+      <div class="toggle_btn">
+        <span></span>
+        <span></span>
+      </div>
+
+      <div id="mask"></div>
+    </header>
+
+   
