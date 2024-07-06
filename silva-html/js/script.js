44293c614e1cfa07037ff664499434e81aa37527
omubose <163929537+omubose@users.noreply.github.com>
2024-07-07 07:55:15 +0900

    Add files via upload

---
+++ silva-html/js/script.js
@@ -0,0 +1,350 @@
+$('#container').scrollgress({//バーの高さの基準となるエリア指定
+  height: '5px',//バーの高さ
+  color: '#168352',//バーの色
+});
+
+
+$('.slider').slick({
+  fade:true,//切り替えをフェードで行う。初期値はfalse。
+  autoplay: true,//自動的に動き出すか。初期値はfalse。
+  autoplaySpeed: 3000,//次のスライドに切り替わる待ち時間
+  speed:1000,//スライドの動きのスピード。初期値は300。
+  infinite: true,//スライドをループさせるかどうか。初期値はtrue。
+  slidesToShow: 1,//スライドを画面に3枚見せる
+  slidesToScroll: 1,//1回のスクロールで3枚の写真を移動して見せる
+  arrows: false,//左右の矢印あり
+  prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
+  nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
+  dots: false,//下部ドットナビゲーションの表示
+      pauseOnFocus: false,//フォーカスで一時停止を無効
+      pauseOnHover: false,//マウスホバーで一時停止を無効
+      pauseOnDotsHover: false,//ドットナビゲーションをマウスホバーで一時停止を無効
+});
+
+//スマホ用：スライダーをタッチしても止めずにスライドをさせたい場合
+$('.slider').on('touchmove', function(event, slick, currentSlide, nextSlide){
+  $('.slider').slick('slickPlay');
+});
+
+
+// 動きのきっかけの起点となるアニメーションの名前を定義
+function BgFadeAnime(){
+
+  // 背景色が伸びて出現（左から右）
+$('.bgLRextendTrigger').each(function(){ //bgLRextendTriggerというクラス名が
+  var elemPos = $(this).offset().top-50;//要素より、50px上の
+  var scroll = $(window).scrollTop();
+  var windowHeight = $(window).height();
+  if (scroll >= elemPos - windowHeight){
+    $(this).addClass('bgLRextend');// 画面内に入ったらbgLRextendというクラス名を追記
+  }else{
+    $(this).removeClass('bgLRextend');// 画面外に出たらbgLRextendというクラス名を外す
+  }
+});	
+
+ // 文字列を囲う子要素
+$('.bgappearTrigger').each(function(){ //bgappearTriggerというクラス名が
+  var elemPos = $(this).offset().top-50;//要素より、50px上の
+  var scroll = $(window).scrollTop();
+  var windowHeight = $(window).height();
+  if (scroll >= elemPos - windowHeight){
+    $(this).addClass('bgappear');// 画面内に入ったらbgappearというクラス名を追記
+  }else{
+    $(this).removeClass('bgappear');// 画面外に出たらbgappearというクラス名を外す
+  }
+});		
+}
+
+// 画面をスクロールをしたら動かしたい場合の記述
+$(window).scroll(function (){
+  BgFadeAnime();/* アニメーション用の関数を呼ぶ*/
+});// ここまで画面をスクロールをしたら動かしたい場合の記述
+
+// 画面が読み込まれたらすぐに動かしたい場合の記述
+$(window).on('load', function(){
+  BgFadeAnime();/* アニメーション用の関数を呼ぶ*/
+});// ここまで画面が読み込まれたらすぐに動かしたい場合の記述
+
+
+
+$('.slider1').slick({
+  arrows: false,//左右の矢印はなし
+  autoplay: true,//自動的に動き出すか。初期値はfalse。
+  autoplaySpeed: 0,//自動的に動き出す待ち時間。初期値は3000ですが今回の見せ方では0
+  speed: 25000,//スライドのスピード。初期値は300。
+  infinite: true,//スライドをループさせるかどうか。初期値はtrue。
+  pauseOnHover: false,//オンマウスでスライドを一時停止させるかどうか。初期値はtrue。
+  pauseOnFocus: false,//フォーカスした際にスライドを一時停止させるかどうか。初期値はtrue。
+  cssEase: 'linear',//動き方。初期値はeaseですが、スムースな動きで見せたいのでlinear
+  slidesToShow: 1.9,//スライドを画面に4枚見せる
+  slidesToScroll: 1,//1回のスライドで動かす要素数
+  responsive: [
+    {
+    breakpoint: 1500,//モニターの横幅が769px以下の見せ方
+    settings: {
+      slidesToShow: 1,//スライドを画面に2枚見せる
+    }
+  },
+    {
+    breakpoint: 769,//モニターの横幅が769px以下の見せ方
+    settings: {
+      slidesToShow: 1,//スライドを画面に2枚見せる
+    }
+  },
+  {
+    breakpoint: 500,//モニターの横幅が426px以下の見せ方
+    settings: {
+      slidesToShow: 0.5,//スライドを画面に1.5枚見せる
+    }
+  }
+]
+});
+
+
+
+$('.slider2').slick({
+  arrows: false,//左右の矢印はなし
+  autoplay: true,//自動的に動き出すか。初期値はfalse。
+  autoplaySpeed: 0,//自動的に動き出す待ち時間。初期値は3000ですが今回の見せ方では0
+  speed: 6900,//スライドのスピード。初期値は300。
+  infinite: true,//スライドをループさせるかどうか。初期値はtrue。
+  pauseOnHover: false,//オンマウスでスライドを一時停止させるかどうか。初期値はtrue。
+  pauseOnFocus: false,//フォーカスした際にスライドを一時停止させるかどうか。初期値はtrue。
+  cssEase: 'linear',//動き方。初期値はeaseですが、スムースな動きで見せたいのでlinear
+  slidesToShow: 2.9,//スライドを画面に4枚見せる
+  slidesToScroll: 1,//1回のスライドで動かす要素数
+  responsive: [
+    {
+    breakpoint: 769,//モニターの横幅が769px以下の見せ方
+    settings: {
+      slidesToShow: 2,//スライドを画面に2枚見せる
+    }
+  },
+  {
+    breakpoint: 426,//モニターの横幅が426px以下の見せ方
+    settings: {
+      slidesToShow: 1.5,//スライドを画面に1.5枚見せる
+    }
+  }
+]
+});
+
+
+// 動きのきっかけの起点となるアニメーションの名前を定義
+function BgFadeAnime() {
+
+  // 背景色が伸びて出現（左から右）
+  $('.bgLRextendTrigger').each(function() {
+    var elemPos = $(this).offset().top - 50; //要素より、50px上の
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    if (scroll >= elemPos - windowHeight) {
+      $(this).addClass('bgLRextend'); // 画面内に入ったらbgLRextendというクラス名を追記
+    } else {
+      $(this).removeClass('bgLRextend'); // 画面外に出たらbgLRextendというクラス名を外す
+    }
+  });
+
+  // 背景色が伸びて出現（上から下）
+  $('.bgTBextendTrigger').each(function() {
+    var elemPos = $(this).offset().top - 50; //要素より、50px上の
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    if (scroll >= elemPos - windowHeight) {
+      $(this).addClass('bgTBextend'); // 画面内に入ったらbgTBextendというクラス名を追記
+    } else {
+      $(this).removeClass('bgTBextend'); // 画面外に出たらbgTBextendというクラス名を外す
+    }
+  });
+
+  // 文字列を囲う子要素
+  $('.bgappearTrigger').each(function() {
+    var elemPos = $(this).offset().top - 50; //要素より、50px上の
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    if (scroll >= elemPos - windowHeight) {
+      $(this).addClass('bgappear'); // 画面内に入ったらbgappearというクラス名を追記
+    } else {
+      $(this).removeClass('bgappear'); // 画面外に出たらbgappearというクラス名を外す
+    }
+  });
+}
+
+// スクロールイベントに対してBgFadeAnime関数を実行
+$(window).on('scroll', function() {
+  BgFadeAnime();
+});
+
+// ページが読み込まれたときにもBgFadeAnime関数を実行
+$(window).on('load', function() {
+  BgFadeAnime();
+});
+
+
+
+
+$('.slider3').slick({
+  arrows: false,//左右の矢印はなし
+  autoplay: true,//自動的に動き出すか。初期値はfalse。
+  autoplaySpeed: 0,//自動的に動き出す待ち時間。初期値は3000ですが今回の見せ方では0
+  speed: 25000,//スライドのスピード。初期値は300。
+  infinite: true,//スライドをループさせるかどうか。初期値はtrue。
+  pauseOnHover: false,//オンマウスでスライドを一時停止させるかどうか。初期値はtrue。
+  pauseOnFocus: false,//フォーカスした際にスライドを一時停止させるかどうか。初期値はtrue。
+  cssEase: 'linear',//動き方。初期値はeaseですが、スムースな動きで見せたいのでlinear
+  slidesToShow: 1.9,//スライドを画面に4枚見せる
+  slidesToScroll: 1,//1回のスライドで動かす要素数
+  // rtl: true, // スライドを左から右に動かす
+  responsive: [
+    {
+    breakpoint: 1500,//モニターの横幅が769px以下の見せ方
+    settings: {
+      slidesToShow: 1,//スライドを画面に2枚見せる
+    }
+  },
+    {
+    breakpoint: 769,//モニターの横幅が769px以下の見せ方
+    settings: {
+      slidesToShow: 1,//スライドを画面に2枚見せる
+    }
+  },
+  {
+    breakpoint: 500,//モニターの横幅が426px以下の見せ方
+    settings: {
+      slidesToShow: 0.5,//スライドを画面に1.5枚見せる
+    }
+  }
+]
+});
+
+
+
+
+
+
+//logoの表示
+$(window).on('load',function(){
+  $("#splash").delay(1500).fadeOut('slow');//ローディング画面を1.5秒（1500ms）待機してからフェードアウト
+  $("#splash_logo").delay(1200).fadeOut('slow');//ロゴを1.2秒（1200ms）待機してからフェードアウト
+
+
+});
+
+$(window).on('load',function(){
+  $("body.no-scroll").delay(2000).queue(function(){
+    $(this).css('overflow', 'visible');
+  })
+});
+
+
+
+
+
+// 画面をスクロールをしたら動かしたい場合の記述
+$(window).scroll(function (){
+  BgFadeAnime();/* アニメーション用の関数を呼ぶ*/
+});// ここまで画面をスクロールをしたら動かしたい場合の記述
+
+// 画面が読み込まれたらすぐに動かしたい場合の記述
+$(window).on('load', function(){
+  BgFadeAnime();/* アニメーション用の関数を呼ぶ*/
+  
+});// ここまで画面が読み込まれたらすぐに動かしたい場合の記述
+
+
+// 画面をスクロールをしたら動かしたい場合の記述
+$(window).scroll(function() {
+  BgFadeAnime(); // アニメーション用の関数を呼ぶ
+});
+
+// 画面が読み込まれたらすぐに動かしたい場合の記述
+$(window).on('load', function() {
+  setTimeout(function() {
+    BgFadeAnime(); // アニメーション用の関数を呼ぶ
+  }, 6000); // 6000ミリ秒（6秒）後に実行
+});
+
+// 動きのきっかけの起点となるアニメーションの名前を定義
+function fadeAnime(){
+  $('.flipRightTopTrigger').each(function(){ //flipRightTopTriggerというクラス名が
+    var elemPos = $(this).offset().top-50;//要素より、50px上の
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    if (scroll >= elemPos - windowHeight){
+    $(this).addClass('flipRightTop');// 画面内に入ったらflipRightTopというクラス名を追記
+    }else{
+    $(this).removeClass('flipRightTop');// 画面外に出たらflipRightTopというクラス名を外す
+    }
+  }); 
+
+  $('.fadeUpTrigger').each(function(){ //fadeUpTriggerというクラス名が
+    var elemPos = $(this).offset().top-10;//要素より、50px上の
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    if (scroll >= elemPos - windowHeight){
+    $(this).addClass('fadeUp');// 画面内に入ったらfadeUpというクラス名を追記
+    }else{
+    $(this).removeClass('fadeUp');// 画面外に出たらfadeUpというクラス名を外す
+    }
+    });
+}
+
+
+
+  // 画面をスクロールをしたら動かしたい場合の記述
+    $(window).scroll(function (){
+      fadeAnime();/* アニメーション用の関数を呼ぶ*/
+    });// ここまで画面をスクロールをしたら動かしたい場合の記述
+  
+  // 画面が読み込まれたらすぐに動かしたい場合の記述
+    $(window).on('load', function(){
+      fadeAnime();/* アニメーション用の関数を呼ぶ*/
+    });// ここまで画面が読み込まれたらすぐに動かしたい場合の記述
+  
+
+
+
+// ドロワーメニュー
+// =============================
+const drawerIcon = jQuery("#js-drawer-icon");
+const drawerContents = jQuery("#js-drawer-content");
+drawerIcon.on("click", function (e) {
+  e.preventDefault();
+  drawerIcon.toggleClass("is-checked");
+  drawerContents.slideToggle(300);
+  jQuery("body").toggleClass("is-fixed");
+});
+
+drawerContents.find("a").on("click", function (e) {
+  // e.preventDefault();
+  drawerIcon.trigger("click");
+});
+
+
+
+$('.slider4').slick({
+  arrows: false,//左右の矢印はなし
+  autoplay: true,//自動的に動き出すか。初期値はfalse。
+  autoplaySpeed: 0,//自動的に動き出す待ち時間。初期値は3000ですが今回の見せ方では0
+  speed: 6900,//スライドのスピード。初期値は300。
+  infinite: true,//スライドをループさせるかどうか。初期値はtrue。
+  pauseOnHover: false,//オンマウスでスライドを一時停止させるかどうか。初期値はtrue。
+  pauseOnFocus: false,//フォーカスした際にスライドを一時停止させるかどうか。初期値はtrue。
+  cssEase: 'linear',//動き方。初期値はeaseですが、スムースな動きで見せたいのでlinear
+  slidesToShow: 3,//スライドを画面に4枚見せる
+  slidesToScroll: 1,//1回のスライドで動かす要素数
+  responsive: [
+    {
+    breakpoint: 769,//モニターの横幅が769px以下の見せ方
+    settings: {
+      slidesToShow: 2,//スライドを画面に2枚見せる
+    }
+  },
+  {
+    breakpoint: 426,//モニターの横幅が426px以下の見せ方
+    settings: {
+      slidesToShow: 1.5,//スライドを画面に1.5枚見せる
+    }
+  }
+]
+});
