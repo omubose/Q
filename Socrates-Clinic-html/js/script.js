694dce637281411de7bff997317dd81db04f9dc6
omubose <163929537+omubose@users.noreply.github.com>
2024-07-13 09:29:37 +0900

    Add files via upload

---
+++ Socrates-Clinic-html/js/script.js
@@ -0,0 +1,117 @@
+//画像の設定
+
+var windowwidth = window.innerWidth || document.documentElement.clientWidth || 0;
+		if (windowwidth > 768){
+			var responsiveImage = [//PC用の画像
+				{ src: '../img/mv1.jpg'},
+				{ src: '../img/mv2.jpg'},
+				{ src: '../img/mv3.jpg'},
+
+
+			];
+		} else {
+			var responsiveImage = [//タブレットサイズ（768px）以下用の画像
+        { src: '../img/mv1.jpg'},
+				{ src: '../img/mv2s.jpg'},
+				{ src: '../img/mv3s.jpg'},
+
+			];
+		}
+
+//Vegas全体の設定
+
+$('#slider').vegas({
+		overlay: false,//画像の上に網線やドットのオーバーレイパターン画像を指定。
+		transition: 'blur',//切り替わりのアニメーション。http://vegas.jaysalvat.com/documentation/transitions/参照。fade、fade2、slideLeft、slideLeft2、slideRight、slideRight2、slideUp、slideUp2、slideDown、slideDown2、zoomIn、zoomIn2、zoomOut、zoomOut2、swirlLeft、swirlLeft2、swirlRight、swirlRight2、burn、burn2、blur、blur2、flash、flash2が設定可能。
+		transitionDuration: 2000,//切り替わりのアニメーション時間をミリ秒単位で設定
+		delay: 10000,//スライド間の遅延をミリ秒単位で。
+		animationDuration: 20000,//スライドアニメーション時間をミリ秒単位で設定
+		animation: 'kenburns',//スライドアニメーションの種類。http://vegas.jaysalvat.com/documentation/transitions/参照。kenburns、kenburnsUp、kenburnsDown、kenburnsRight、kenburnsLeft、kenburnsUpLeft、kenburnsUpRight、kenburnsDownLeft、kenburnsDownRight、randomが設定可能。
+		slides: responsiveImage,//画像設定を読む
+		timer:false,// プログレスバーを非表示したい場合はこのコメントアウトを外してください
+	});
+
+
+
+
+	$(function () {
+		$('.slider1').slick({
+			autoplay: true,
+			autoplaySpeed: 0,
+			arrows: false,
+			cssEase: 'linear',
+			pauseOnHover: false,
+			speed: 20000,
+			swipe: false,
+			touchMove: false,
+			variableWidth: true,
+		});
+
+});
+	$(function () {
+		$('.slider2').slick({
+			autoplay: true,
+			autoplaySpeed: 0,
+			arrows: false,
+			cssEase: 'linear',
+			pauseOnHover: false,
+			speed: 100000,
+			swipe: false,
+			touchMove: false,
+			variableWidth: true,
+		});
+
+});
+
+
+// ドロワーメニュー
+// =============================
+const drawerIcon = jQuery("#js-drawer-icon");
+const drawerContents = jQuery("#js-drawer-content");
+drawerIcon.on("click", function (e) {
+  e.preventDefault();
+  drawerIcon.toggleClass("is-checked");
+  drawerContents.slideToggle(300);
+  jQuery("body").toggleClass("is-fixed");
+});
+
+drawerContents.find("a").on("click", function (e) {
+  // e.preventDefault();
+  drawerIcon.trigger("click");
+});
+
+
+
+jQuery('a[href^="#"]').on("click", function (event) {
+  event.preventDefault(); // デフォルトの動作をキャンセル
+
+  const header = jQuery("#js-header");
+  const headerHeight = header.innerHeight();
+  const speed = 500;
+  const id = jQuery(this).attr("href");
+  let target = jQuery("html"); // デフォルトのターゲットはhtml
+  let position = 0; // デフォルトのスクロール位置は0
+
+  if (id !== "#" && id !== "") {
+    target = jQuery(id);
+    position = target.offset().top - headerHeight;
+
+    // ヘッダーがfixedでない場合の調整
+    if ("fixed" !== header.css("position")) {
+      position = target.offset().top;
+    }
+  }
+
+  // スクロール位置が負の場合は0に設定
+  if (position < 0) {
+    position = 0;
+  }
+
+  jQuery("html, body").animate(
+    {
+      scrollTop: position,
+    },
+    speed,
+    "swing"
+  );
+});
